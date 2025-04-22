e18f20fecab9dc3b1333b078909a7cbe655f1cb7
VietNV <vietnv1304@gmail.com>
2025-04-22 23:39:11 +0700

    update config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1dd26751887e80989a3eedd8170800ec',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Kyber Group Careers',
+  domain: 'kybernetwork.notion.site',
+  author: 'Kyber Network',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description:
+    'Job opportunities with an industry-leading blockchain company. Please send your resume to email: hr@kyber.network.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'KyberNetwork',
+  // github: 'transitive-bullshit',
+  linkedin: 'company/kybernetwork',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
